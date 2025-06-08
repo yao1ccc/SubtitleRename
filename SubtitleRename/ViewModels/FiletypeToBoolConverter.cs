ac11e78e2423f2f03fd5990220a5be32957ddeeb
yao1ccc <yao1ccc@outlook.com>
2025-06-08 18:02:56 +0800

    ▷

--- SubtitleRename/ViewModels/FiletypeToBoolConverter.cs
+++ SubtitleRename/ViewModels/FiletypeToBoolConverter.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using SubtitleRename.Models;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
-using SubtitleRename.Models;
 
 namespace SubtitleRename.ViewModels
 {
